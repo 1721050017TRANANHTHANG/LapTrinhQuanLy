cdf4387cd4fa28b94e7cfcdf7cd5234a02c250f7
DELL <DELL@DESKTOP-CHRBT3H>
2021-08-31 13:30:10 +0700

    abc

---
+++ BaiTap004/BaiTap004/Models/GiaiPhuongTrinh.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap004.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public double SoA(get; set;)
+        public double SoB(get; set;)
+            public double GiaiPhuongTrinh(double SoA, double SoB)
+        {
+            double x = 0;
+            x = -SoB / SoA;
+            return x;
+        }
+
+
+    }
+}
