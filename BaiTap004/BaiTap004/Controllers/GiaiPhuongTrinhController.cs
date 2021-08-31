6cca157e6880a57c97409380b54fe09a993eba4d
DELL <DELL@DESKTOP-CHRBT3H>
2021-08-31 13:43:47 +0700

    abc

---
+++ BaiTap004/BaiTap004/Controllers/GiaiPhuongTrinhController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiTap004.Models;
+namespace BaiTap004.Controllers
+{
+    public class GiaiPhuongTrinhController : Controller
+    {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
+        // GET: GiaiPhuongTrinh
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index ( string soThuNhat , string soThuHai)
+        {
+            gpt.SoA = convert.ToInt32(soThuNhat);
+            gpt.SoB = convert.ToInt32(soThuHai);
+            ViewBag.x = gpt.x(gpt.SoA, gpt.SoB);
+            return View();
+
+        }
+
+
+    }
+}
