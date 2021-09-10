3b544d9181c774cc70e5225c2329b4e37fe1ce7e
DELL <DELL@DESKTOP-CHRBT3H>
2021-09-10 14:17:36 +0700

    abc

---
+++ KetNoi/KetNoi/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KetNoi.Models
+{
+    public class Person
+    {
+        public string PersonID { get; set; }
+        public string PersonName { get; set; }
+
+    }
+}
