b9692ca5c8c64da9ecfe912942838d6a4362e4af
DELL <DELL@DESKTOP-CHRBT3H>
2021-09-14 14:32:05 +0700

    abc

---
+++ LuyenTap/LuyenTap/Models/AutoGenerateKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuyenTap.Models
+{
+    public class AutoGenerateKey
+    {
+        public string GenerateKey(string id)
+        {
+            string strkey = "";
+            return strkey;
+        }
+    }
+}
