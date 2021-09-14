8eceb1b8c9e7c54c983bb7ed3fc22d225673c56b
DELL <DELL@DESKTOP-CHRBT3H>
2021-09-14 12:37:32 +0700

    abc

---
+++ LuyenTap/LuyenTap/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace LuyenTap.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LuyenTap.Models.LTQLDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LuyenTap.Models.LTQLDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ LuyenTap/LuyenTap/Models/LTQLDbContext.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LuyenTap.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext()
+            : base("name=LTQLDbContext")
+        {
+        }
+
+       
+        public virtual DbSet<KhachHang> KhachHangs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<KhachHang>()
+                .Property(e => e.SoDienThoai)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<KhachHang>()
+                .Property(e => e.MaKhachHang)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<KhachHang>()
+                .Property(e => e.TenKhachHang)
+                .IsUnicode(false);
+        }
+    }
+}
